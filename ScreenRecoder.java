13492ee611e015153925ff6e2a1629a97d24c2e5
Pratik Patel <pmp2@illinois.edu>
2022-05-15 21:08:09 -0500

    Removing commented out code

--- ScreenRecoder.java
+++ ScreenRecoder.java
@@ -64,37 +64,8 @@ public class ScreenRecoder
         catch(Exception e) { System.out.println(e); }
     }
 
-    public static void makeVideo() throws Exception { Runtime.getRuntime().exec("cmd /c start cmd.exe /k \"python video_maker.py && exit\"").waitFor(); }
-}
-
-/*
-
-import java.awt.AWTException;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
- 
-import javax.imageio.ImageIO;
- 
-public class FullScreenCaptureExample {
- 
-    public static void main(String[] args) {
-        try {
-            Robot robot = new Robot();
- 
-            Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-            BufferedImage bufferedImage = robot.createScreenCapture(rectangle);
-            File file = new File("screen-capture.png");
-            boolean status = ImageIO.write(bufferedImage, "png", file);
-            System.out.println("Screen Captured ? " + status + " File Path:- " + file.getAbsolutePath());
- 
-        } catch (AWTException | IOException ex) {
-            System.err.println(ex);
-        }
+    public static void makeVideo() throws Exception 
+    { 
+        Runtime.getRuntime().exec("cmd /c start cmd.exe /k \"python video_maker.py && exit\"").waitFor(); 
     }
 }
-
-*/
